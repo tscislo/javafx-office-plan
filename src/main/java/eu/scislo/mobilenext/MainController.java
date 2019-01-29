9b4dffc9c43f84f0f51efdd602acf35eaf004b3c
Tomasz Ścisło <scislot@gmail.com>
2019-01-29 01:16:31 +0100

    It is possible to add new person

--- src/main/java/eu/scislo/mobilenext/MainController.java
+++ src/main/java/eu/scislo/mobilenext/MainController.java
@@ -1,17 +1,19 @@
 package eu.scislo.mobilenext;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTreeTableColumn;
-import com.jfoenix.controls.JFXTreeTableView;
-import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import com.jfoenix.validation.NumberValidator;
+import com.jfoenix.validation.RequiredFieldValidator;
+import de.jensd.fx.glyphs.GlyphsBuilder;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 
 import java.net.URL;
@@ -20,7 +22,6 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
-    public Person tomasz = new Person("Tomasz", "Ścisło", "125");
 
     @FXML
     public JFXButton read;
@@ -46,6 +47,22 @@ public class MainController implements Initializable {
     @FXML
     private JFXTreeTableView<Person> peopleList;
 
+    @FXML
+    private JFXTextField name;
+
+    @FXML
+    private JFXTextField surname;
+
+    @FXML
+    private JFXTextField roomNo;
+
+    @FXML
+    private JFXTextField startTime;
+
+    @FXML
+    private JFXTextField endTime;
+
+    public Person selectedPerson = new Person();
     private ObservableList<Person> people = FXCollections.observableArrayList();
 
     @Override
@@ -60,21 +77,77 @@ public class MainController implements Initializable {
         roomColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<Person, String> param) -> param.getValue().getValue().roomProperty());
 
 
-        this.people.add(tomasz);
-        this.people.add(tomasz);
-        this.people.add(tomasz);
-        this.people.add(tomasz);
-        this.people.add(tomasz);
-
-
         peopleList.setRoot(new RecursiveTreeItem<>(people, RecursiveTreeObject::getChildren));
         peopleList.setShowRoot(false);
         peopleList.getColumns().setAll(firstNameColumn, lastNameColumn, roomColumn);
 
+        this.addRequiredValidator(this.name, "Podaj imię!");
+        this.addRequiredValidator(this.surname, "Podaj nazwisko!");
+        this.addRequiredValidator(this.endTime, "Podaj godzinę!");
+        this.addRequiredValidator(this.startTime, "Podaj godzinę!");
+        this.addRequiredValidator(this.roomNo, "Podaj pokój!");
+
+        this.addNumberValidator(this.roomNo, "Musi być liczbą!");
+        this.addNumberValidator(this.endTime, "Musi być liczbą!");
+        this.addNumberValidator(this.startTime, "Musi być liczbą!");
+
+        this.name.textProperty().bindBidirectional(selectedPerson.firstNameProperty());
+        this.surname.textProperty().bindBidirectional(selectedPerson.lastNameProperty());
+        this.startTime.textProperty().bindBidirectional(selectedPerson.startTimeProperty());
+        this.endTime.textProperty().bindBidirectional(selectedPerson.endTimeProperty());
+        this.roomNo.textProperty().bindBidirectional(selectedPerson.roomProperty());
+
+        // Enables/disables add button
+        this.add.disableProperty().bind(Bindings.createBooleanBinding(
+                () ->
+                        !this.name.validate() ||
+                                !this.startTime.validate() ||
+                                !this.surname.validate() ||
+                                !this.endTime.validate() ||
+                                !this.roomNo.validate(),
+                this.selectedPerson.roomProperty(),
+                this.selectedPerson.startTimeProperty(),
+                this.selectedPerson.endTimeProperty(),
+                this.selectedPerson.firstNameProperty(),
+                this.selectedPerson.lastNameProperty()
+        ));
+
+    }
+
+    private void addRequiredValidator(JFXTextField textField, String customValidationText) {
+        RequiredFieldValidator validator = new RequiredFieldValidator();
+        validator.setMessage(customValidationText);
+        validator.setIcon(GlyphsBuilder.create(FontAwesomeIconView.class)
+                .glyph(FontAwesomeIcon.WARNING)
+                .size("1em")
+                .styleClass("error")
+                .build());
+        textField.getValidators().add(validator);
+        textField.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) {
+                textField.validate();
+            }
+        });
+    }
+
+    private void addNumberValidator(JFXTextField textField, String customValidationText) {
+        NumberValidator validator = new NumberValidator();
+        validator.setMessage(customValidationText);
+        validator.setIcon(GlyphsBuilder.create(FontAwesomeIconView.class)
+                .glyph(FontAwesomeIcon.WARNING)
+                .size("1em")
+                .styleClass("error")
+                .build());
+        textField.getValidators().add(validator);
     }
 
     public void rename() {
-        this.tomasz.setFirstName("Karol");
+
+    }
+
+    public void add() {
+        this.people.add(this.selectedPerson.clone());
+
     }
 
 
--- src/main/java/eu/scislo/mobilenext/Person.java
+++ src/main/java/eu/scislo/mobilenext/Person.java
@@ -3,11 +3,18 @@ package eu.scislo.mobilenext;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import javafx.beans.property.SimpleStringProperty;
 
+
 public class Person extends RecursiveTreeObject<Person> {
     private SimpleStringProperty firstName = new SimpleStringProperty();
     private SimpleStringProperty lastName = new SimpleStringProperty();
     private SimpleStringProperty room = new SimpleStringProperty();
+    private SimpleStringProperty startTime = new SimpleStringProperty();
+    private SimpleStringProperty endTime = new SimpleStringProperty();
 
+
+    public Person() {
+
+    }
 
     public Person(String firstName, String lastName, String room) {
         this.setRoom(room);
@@ -54,7 +61,46 @@ public class Person extends RecursiveTreeObject<Person> {
     }
 
 
+    public String getStartTime() {
+        return startTime.get();
+    }
+
+    public SimpleStringProperty startTimeProperty() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime.set(startTime);
+    }
+
+
+    public String getEndTime() {
+        return endTime.get();
+    }
+
+    public SimpleStringProperty endTimeProperty() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime.set(endTime);
+    }
+
+
     public String toString() {
         return this.getFirstName() + " " + this.getLastName() + " " + this.getRoom();
+    }
+
+    public Person clone() {
+        Person cloned = new Person();
+
+        // clone mutable fields
+        cloned.setFirstName(this.getFirstName());
+        cloned.setLastName(this.getLastName());
+        cloned.setRoom(this.getRoom());
+        cloned.setStartTime(this.getStartTime());
+        cloned.setEndTime(this.getEndTime());
+
+        return cloned;
     }
 }
